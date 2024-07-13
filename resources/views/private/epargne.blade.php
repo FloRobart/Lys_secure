e49861d416fc997500e5ea0dfdce130397e3bbda
FloRobart <FlorisRobart.pro@gmail.com>
2024-07-13 02:42:44 +0200

    ajout de la modification de salaire

--- resources/views/private/epargne.blade.php
+++ resources/views/private/epargne.blade.php
@@ -48,11 +48,11 @@
             <!-- Entête du tableau -->
             <thead class="w-full">
                 <tr class="tableRow smallText text-center font-bold">
-                    <th class="w-4/12 mx-2 min-[600px]:px-4">Date du virement</th>
-                    <th class="w-4/12 mx-2 min-[600px]:px-4">Montant épargné</th>
-                    <th class="w-4/12 mx-2 min-[600px]:px-4">Nom de la banque</th>
-                    <th class="w-4/12 mx-2 min-[600px]:px-4">Nom du compte</th>
-                    <th class="w-4/12 mx-2 min-[600px]:px-4">Actions</th>
+                    <th class="tableCell">Date du virement</th>
+                    <th class="tableCell">Montant épargné</th>
+                    <th class="tableCell">Nom de la banque</th>
+                    <th class="tableCell">Nom du compte</th>
+                    <th class="tableCell">Actions</th>
                 </tr>
             </thead>
 
@@ -61,13 +61,31 @@
                 @if (isset($epargnes))
                     @foreach ($epargnes as $epargne)
                         <tr class="tableRow smallText text-center">
-                            <td class="w-4/12 mx-2 min-[600px]:px-4" title="{{ strftime('%A %d %B %Y',strtotime($epargne->date_transaction)); }}">{{ strftime('%d %B %Y',strtotime($epargne->date_transaction)); }}</td>
-                            <td class="w-4/12 mx-2 min-[600px]:px-4" title="{{ number_format($epargne->montant_transaction, 2, ',', ' ') }} €">{{ number_format($epargne->montant_transaction, 2, ',', ' ') }} €</td>
-                            <td class="w-4/12 mx-2 min-[600px]:px-4" title="{{ $epargne->banque }}">{{ $epargne->banque }}</td>
-                            <td class="w-4/12 mx-2 min-[600px]:px-4" title="{{ $epargne->compte }}">{{ $epargne->compte }}</td>
-                            <td class="flex flex-row w-4/12 mx-2 min-[500px]:px-4 py-2">
-                                <a href="{{ route('removeEpargne', $epargne->id) }}" onclick="return confirm('Êtes-vous sûr de vouloir supprimer l\'épargne du {{ strftime('%A %d %B %Y',strtotime($epargne->date_transaction)); }} ? Cette action est irréversible.')" class="smallTextReverse font-bold bgError hover:bgErrorFonce focus:normalScale rounded-xl py-1 px-2 mx-px min-[500px]:mx-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <!-- Date de la transaction -->
+                            <td class="tableCell" title="{{ strftime('%A %d %B %Y',strtotime($epargne->date_transaction)); }}">{{ strftime('%d %B %Y',strtotime($epargne->date_transaction)); }}</td>
+                            
+                            <!-- Montant de la transaction -->
+                            <td class="tableCell" title="{{ number_format($epargne->montant_transaction, 2, ',', ' ') }} €">{{ number_format($epargne->montant_transaction, 2, ',', ' ') }} €</td>
+                            
+                            <!-- Nom de la banque -->
+                            <td class="tableCell" title="{{ $epargne->banque }}">{{ $epargne->banque }}</td>
+                            
+                            <!-- Nom du compte -->
+                            <td class="tableCell" title="{{ $epargne->compte }}">{{ $epargne->compte }}</td>
+
+                            <!-- Actions -->
+                            <!-- Actions -->
+                            <td class="smallRowCenterContainer px-1 min-[460px]:px-2 min-[500px]:px-4 py-2">
+                                <!-- Modifier -->
+                                <a href="{{ route('removeEpargne', $epargne->id) }}" onclick="return confirm('Êtes-vous sûr de vouloir supprimer le salaire du {{ strftime('%A %d %B %Y',strtotime($salaire->date_transaction)); }} ? Cette action est irréversible.')" class="smallRowCenterContainer w-fit smallTextReverse font-bold bgBleuLogo hover:bgBleuFonce focus:normalScale rounded-lg min-[500px]:rounded-xl py-1 px-1 min-[500px]:px-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="tinySizeIcons">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                    </svg>
+                                </a>
+
+                                <!-- Supprimer -->
+                                <a href="{{ route('removeEpargne', $epargne->id) }}" onclick="return confirm('Êtes-vous sûr de vouloir supprimer le salaire du {{ strftime('%A %d %B %Y',strtotime($salaire->date_transaction)); }} ? Cette action est irréversible.')" class="smallRowCenterContainer w-fit smallTextReverse font-bold bgError hover:bgErrorFonce focus:normalScale rounded-lg min-[500px]:rounded-xl py-1 px-1 min-[500px]:px-2 ml-1 min-[500px]:ml-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="tinySizeIcons">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                     </svg>
                                 </a>
@@ -83,10 +101,10 @@
             @csrf
             <div class="colCenterContainer">
                 <div class="colStartContainer sm:rowStartContainer">
-                    <input name="date_transaction" required type="date" value="{{ date('Y-m-d') }}" max="{{ date('Y-m-d') }}" class="w-[55%] sm:w-4/12 mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
-                    <input name="montant_transaction" required type="number" step="0.01" placeholder="Montant du versement" min="0" class="w-[55%] sm:w-4/12 mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
-                    <input name="banque" required type="text" placeholder="Nom de la banque" class="w-[55%] sm:w-4/12 mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
-                    <input name="compte" required type="text" placeholder="Nom du compte" class="w-[55%] sm:w-4/12 mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
+                    <input id="date_transaction"    name="date_transaction"    required type="date" value="{{ date('Y-m-d') }}" max="{{ date('Y-m-d') }}" class="w-[55%] sm:w-4/12 mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
+                    <input id="montant_transaction" name="montant_transaction" required type="number" step="0.01" placeholder="Montant du versement" min="0" class="w-[55%] sm:w-4/12 mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
+                    <input id="banque"              name="banque"              required type="text" placeholder="Nom de la banque" class="w-[55%] sm:w-4/12 mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
+                    <input id="compte"              name="compte"              required type="text" placeholder="Nom du compte" class="w-[55%] sm:w-4/12 mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
                 </div>
                 <button class="buttonForm mx-2 min-[500px]:mx-4 my-2">Ajouter</button>
                 <div class="w-full tableRowTop"></div>

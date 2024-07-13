8f571bbeb4d141a5c3558054d34e46d1d15d2ca8
FloRobart <FlorisRobart.pro@gmail.com>
2024-07-13 03:01:08 +0200

    ajout de la modification d'épargne

--- resources/views/private/epargne.blade.php
+++ resources/views/private/epargne.blade.php
@@ -74,17 +74,16 @@
                             <td class="tableCell" title="{{ $epargne->compte }}">{{ $epargne->compte }}</td>
 
                             <!-- Actions -->
-                            <!-- Actions -->
                             <td class="smallRowCenterContainer px-1 min-[460px]:px-2 min-[500px]:px-4 py-2">
                                 <!-- Modifier -->
-                                <a href="{{ route('removeEpargne', $epargne->id) }}" onclick="return confirm('Êtes-vous sûr de vouloir supprimer le salaire du {{ strftime('%A %d %B %Y',strtotime($salaire->date_transaction)); }} ? Cette action est irréversible.')" class="smallRowCenterContainer w-fit smallTextReverse font-bold bgBleuLogo hover:bgBleuFonce focus:normalScale rounded-lg min-[500px]:rounded-xl py-1 px-1 min-[500px]:px-2">
+                                <button onclick="editEpargne('{{ strftime('%Y-%m-%d', strtotime($epargne->date_transaction)) }}', '{{ $epargne->montant_transaction }}', '{{ $epargne->banque }}', '{{ $epargne->compte }}', '{{ $epargne->id }}')" class="smallRowCenterContainer w-fit smallTextReverse font-bold bgBleuLogo hover:bgBleuFonce focus:normalScale rounded-lg min-[500px]:rounded-xl py-1 px-1 min-[500px]:px-2">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="tinySizeIcons">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                     </svg>
-                                </a>
+                                </button>
 
                                 <!-- Supprimer -->
-                                <a href="{{ route('removeEpargne', $epargne->id) }}" onclick="return confirm('Êtes-vous sûr de vouloir supprimer le salaire du {{ strftime('%A %d %B %Y',strtotime($salaire->date_transaction)); }} ? Cette action est irréversible.')" class="smallRowCenterContainer w-fit smallTextReverse font-bold bgError hover:bgErrorFonce focus:normalScale rounded-lg min-[500px]:rounded-xl py-1 px-1 min-[500px]:px-2 ml-1 min-[500px]:ml-2">
+                                <a href="{{ route('removeEpargne', $epargne->id) }}" onclick="return confirm('Êtes-vous sûr de vouloir supprimer le salaire du {{ strftime('%A %d %B %Y',strtotime($epargne->date_transaction)) }} ? Cette action est irréversible.')" class="smallRowCenterContainer w-fit smallTextReverse font-bold bgError hover:bgErrorFonce focus:normalScale rounded-lg min-[500px]:rounded-xl py-1 px-1 min-[500px]:px-2 ml-1 min-[500px]:ml-2">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="tinySizeIcons">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                     </svg>
@@ -106,7 +105,7 @@
                     <input id="banque"              name="banque"              required type="text" placeholder="Nom de la banque" class="w-[55%] sm:w-4/12 mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
                     <input id="compte"              name="compte"              required type="text" placeholder="Nom du compte" class="w-[55%] sm:w-4/12 mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
                 </div>
-                <button class="buttonForm mx-2 min-[500px]:mx-4 my-2">Ajouter</button>
+                <button id="formButton" class="buttonForm mx-2 min-[500px]:mx-4 my-2">Ajouter</button>
                 <div class="w-full tableRowTop"></div>
             </div>
         </form>
@@ -119,4 +118,31 @@
 
 @section('scripts')
 <script src="{{ asset('js/showForm.js') }}"></script>
+<script>
+    oldId = 0;
+    /* Fonction pour modifier un salaire */
+    function editEpargne(date, montant, banque, compte, id) {
+        /* Affichage du formulaire */
+        hidden = document.getElementById('form').classList.contains('hidden');
+        if (hidden || oldId == id) {
+            showForm('Ajouter une épargne', 'Modifier', '{{ route('editEpargne') }}');
+        } else {
+            document.getElementById('formButton').innerText = 'Modifier';
+            document.getElementById('form').action = '{{ route('editEpargne') }}';
+        }
+
+        /* Remplissage du formulaire */
+        document.getElementById('date_transaction').value = date;
+        document.getElementById('montant_transaction').value = montant;
+        document.getElementById('banque').value = banque;
+        document.getElementById('compte').value = compte;
+
+        if (document.getElementById('id') != null) {
+            document.getElementById('id').remove();
+        }
+        document.getElementById('form').insertAdjacentHTML('beforeend', '<input type="hidden" id="id" name="id" value="' + id + '">');
+
+        oldId = id;
+    }
+</script>
 @endsection

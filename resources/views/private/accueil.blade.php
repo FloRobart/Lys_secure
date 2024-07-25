f7700b44589c7f2449478d692b0e4df3477d4f8b
FloRobart <FlorisRobart.pro@gmail.com>
2024-07-25 17:24:27 +0200

    correction de l'accueil

--- resources/views/private/accueil.blade.php
+++ resources/views/private/accueil.blade.php
@@ -25,22 +25,29 @@
 
 <!-- Affichage des différents profils -->
 <section class="colCenterContainer gap-y-10 bgPage mb-[21rem] min-[400px]:mb-68 md:mb-[30rem] lg:mb-[21rem] xl:mb-52">
-    <a href="{{ route('salaires') }}" class="buttonForm">Revenue</a>
-    <a href="{{ route('epargnes') }}" class="buttonForm">Épargnes</a>
-    <a href="{{ route('abonnements') }}" class="buttonForm">Abonnements</a>
-    <a href="{{ route('emprunts') }}" class="buttonForm">Emprunts</a>
-    <a href="{{ route('depenses') }}" class="buttonForm">Dépenses</a>
-    <a href="{{ route('prets') }}" class="buttonForm">Prêts</a>
+    <!-- Informations diverses -->
+    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 text-center">
+        <a href="{{ route('salaires') }}"    class="buttonForm">Revenue    </a>
+        <a href="{{ route('epargnes') }}"    class="buttonForm">Épargnes   </a>
+        <a href="{{ route('depenses') }}"    class="buttonForm">Dépenses   </a>
+        <a href="{{ route('abonnements') }}" class="buttonForm">Abonnements</a>
+        <a href="{{ route('emprunts') }}"    class="buttonForm">Emprunts   </a>
+        <a href="{{ route('prets') }}"       class="buttonForm">Prêts      </a>
+    </div>
 
-    <h1 class="titleText">Investissements</h1>
-    <a href="{{ route('investissements') }}" class="buttonForm">Tous les investissements</a>
+    <!-- Investissements -->
+    <h1 class="titleText mt-12"><a href="{{ route('investissements') }}" class="link">Investissements</a></h1>
 
+    <!-- Affichage des différents types d'investissements -->
     @if (isset($investissements))
-        @foreach ($investissements as $investissement)
-            <a href="{{ route('investissements.type', $investissement->type_investissement) }}" class="buttonForm">{{ ucfirst($investissement->type_investissement) }}</a>
-        @endforeach
+        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 text-center">
+            @foreach ($investissements as $investissement)
+                <a href="{{ route('investissements.type', $investissement->type_investissement) }}" class="buttonForm">{{ ucfirst($investissement->type_investissement) }}</a>
+            @endforeach
+        </div>
     @endif
 
+    <!-- Formulaire d'ajout d'un type d'investissement -->
     <form id="addInvestissementTypeForm" action="{{ route('investissement.type.add') }}" method="POST" class="w-9/12 sm:w-6/12 colCenterContainer gap-y-4 hidden">
         @csrf
         <label for="new_type" class="normalText text-center">Nom du nouveau type d'investissement</label>
@@ -51,7 +58,7 @@
         </div>
     </form>
 
-    <button onclick="showAddInvestissementTypeForm()" id="addInvestissementTypeButton" class="buttonForm">+ Ajoutez un investissement</button>
+    <button onclick="showAddInvestissementTypeForm()" id="addInvestissementTypeButton" class="buttonForm mt-8">+ Ajoutez un investissement</button>
 </section>
 @endsection
 

fa6023a6fd58153300d54631735c5c7f67adcfef
FloRobart <FlorisRobart.pro@gmail.com>
2024-07-26 23:08:29 +0200

    correction de bug mineur

--- resources/views/private/investissement.blade.php
+++ resources/views/private/investissement.blade.php
@@ -148,12 +148,14 @@
                             
                             <!-- Actions -->
                             <td class="smallRowCenterContainer px-1 min-[460px]:px-2 min-[500px]:px-4 py-2">
-                                <!-- Modifier -->
-                                <button onclick="editInvestissement('{{ strftime('%Y-%m-%d', strtotime($investissement->date_transaction)) }}', '{{ str_replace('\'', '\\\'', $investissement->nom_actif) }}', '{{ $investissement->montant_transaction }}', '{{ $investissement->frais_transaction }}', '{{ $investissement->id }}')" class="smallRowCenterContainer w-fit smallTextReverse font-bold bgBleuLogo hover:bgBleuFonce focus:normalScale rounded-lg min-[500px]:rounded-xl py-1 px-1 min-[500px]:px-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="tinySizeIcons">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                                    </svg>
-                                </button>
+                                @if (!str_contains(strtolower(URL::current()), 'type/investissements'))
+                                    <!-- Modifier -->
+                                    <button onclick="editInvestissement('{{ strftime('%Y-%m-%d', strtotime($investissement->date_transaction)) }}', '{{ str_replace('\'', '\\\'', $investissement->nom_actif) }}', '{{ $investissement->montant_transaction }}', '{{ $investissement->frais_transaction }}', '{{ $investissement->id }}')" class="smallRowCenterContainer w-fit smallTextReverse font-bold bgBleuLogo hover:bgBleuFonce focus:normalScale rounded-lg min-[500px]:rounded-xl py-1 px-1 min-[500px]:px-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="tinySizeIcons">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                        </svg>
+                                    </button>
+                                @endif
 
                                 <!-- Supprimer -->
                                 <a href="{{ route('investissement.remove', $investissement->id) }}" onclick="return confirm('Êtes-vous sûr de vouloir supprimer l\'investissement en {{ str_replace('\'', '\\\'', $investissement->type_investissement_investissement) }} du {{ strftime('%A %d %B %Y',strtotime($investissement->date_transaction)) }} ? Cette action est irréversible.')" class="smallRowCenterContainer w-fit smallTextReverse font-bold bgError hover:bgErrorFonce focus:normalScale rounded-lg min-[500px]:rounded-xl py-1 px-1 min-[500px]:px-2 ml-1 min-[500px]:ml-2">

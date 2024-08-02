605d30155cd406a4cf25f2aeeadbc28c0c3d6418
FloRobart <FlorisRobart.pro@gmail.com>
2024-08-02 17:22:12 +0200

    ajout des titres

--- resources/views/private/compte.blade.php
+++ resources/views/private/compte.blade.php
@@ -41,7 +41,12 @@
 
         <!-- Nombre d'email différents -->
         <div class="rowCenterContainer">
-            <span class="normalText">Nombre d'email différents : <span class="normalTextBleuLogo font-bold">{{ $comptes->unique('email')->count() }}</span></span>
+            <span class="normalText">Nombre d'identifiant différents : <span class="normalTextBleuLogo font-bold">{{ $comptes->unique('email')->count() }}</span></span>
+        </div>
+
+        <!-- Nombre d'email différents -->
+        <div class="rowCenterContainer">
+            <span class="normalText">Nombre d'email différents : <span class="normalTextBleuLogo font-bold">{{ $comptes->where('name', 'Gmail')->count() }}</span></span>
         </div>
     </div>
 
@@ -70,45 +75,45 @@
                         <tr class="tableRow smallText text-center">
                             <!-- Nom du compte -->
                             @if (str_contains(strtolower(URL::current()), 'name'))
-                                <td class="tableCell"><a href="{{ route('comptes.name', ['name' => $compte->name]) }}" class="link">{{ $compte->name }}</a></td>
+                                <td class="tableCell"><a title="Afficher les comptes {{ $compte->name }}" href="{{ route('comptes.name', ['name' => $compte->name]) }}" class="link">{{ $compte->name }}</a></td>
                             @else
                                 @if (str_contains(strtolower(URL::current()), 'email'))
                                     @if (str_contains(strtolower(URL::current()), 'pseudo'))
-                                        <td class="tableCell" title="Afficher les"><a href="{{ route('comptes.name.email.pseudo', ['name' => $compte->name, 'email' => $compte->email, 'pseudo' => $compte->pseudo]) }}" class="link">{{ $compte->name }}</a></td>
+                                        <td class="tableCell" title="Afficher les comptes {{ $compte->name }} lié au mail {{ $compte->email }} avec le pseudo {{ $compte->pseudo }}"><a href="{{ route('comptes.name.email.pseudo', ['name' => $compte->name, 'email' => $compte->email, 'pseudo' => $compte->pseudo]) }}" class="link">{{ $compte->name }}</a></td>
                                     @else
-                                        <td class="tableCell" title="Afficher les"><a href="{{ route('comptes.name.email', ['name' => $compte->name, 'email' => $compte->email]) }}" class="link">{{ $compte->name }}</a></td>
+                                        <td class="tableCell" title="Afficher les comptes {{ $compte->name }} lié au mail {{ $compte->email }}"><a href="{{ route('comptes.name.email', ['name' => $compte->name, 'email' => $compte->email]) }}" class="link">{{ $compte->name }}</a></td>
                                     @endif
                                 @else
                                     @if (str_contains(strtolower(URL::current()), 'pseudo'))
-                                        <td class="tableCell" title="Afficher les"><a href="{{ route('comptes.name.pseudo', ['name' => $compte->name, 'pseudo' => $compte->pseudo]) }}" class="link">{{ $compte->name }}</a></td>
+                                        <td class="tableCell" title="Afficher les comptes {{ $compte->name }} avec le pseudo {{ $compte->pseudo }}"><a href="{{ route('comptes.name.pseudo', ['name' => $compte->name, 'pseudo' => $compte->pseudo]) }}" class="link">{{ $compte->name }}</a></td>
                                     @else
-                                        <td class="tableCell" title="Afficher les"><a href="{{ route('comptes.name', ['name' => $compte->name]) }}" class="link">{{ $compte->name }}</a></td>
+                                        <td class="tableCell" title="Afficher les comptes {{ $compte->name }}"><a href="{{ route('comptes.name', ['name' => $compte->name]) }}" class="link">{{ $compte->name }}</a></td>
                                     @endif
                                 @endif
                             @endif
                             
                             <!-- Email -->
                             @if (str_contains(strtolower(URL::current()), 'email'))
-                                <td class="tableCell"><a href="{{ route('comptes.email', ['email' => $compte->email]) }}" class="link">{{ $compte->email }}</a></td>
+                                <td class="tableCell"><a title="Afficher les comptes lié au mail {{ $compte->email }}" href="{{ route('comptes.email', ['email' => $compte->email]) }}" class="link">{{ $compte->email }}</a></td>
                             @else
                                 @if (str_contains(strtolower(URL::current()), 'name'))
                                     @if (str_contains(strtolower(URL::current()), 'pseudo'))
-                                        <td class="tableCell" title="Afficher les"><a href="{{ route('comptes.name.email.pseudo', ['name' => $compte->name, 'email' => $compte->email, 'pseudo' => $compte->pseudo]) }}" class="link">{{ $compte->email }}</a></td>
+                                        <td class="tableCell" title="Afficher les comptes {{ $compte->name }} lié au mail {{ $compte->email }} avec le pseudo {{ $compte->pseudo }}"><a href="{{ route('comptes.name.email.pseudo', ['name' => $compte->name, 'email' => $compte->email, 'pseudo' => $compte->pseudo]) }}" class="link">{{ $compte->email }}</a></td>
                                     @else
-                                        <td class="tableCell" title="Afficher les"><a href="{{ route('comptes.name.email', ['name' => $compte->name, 'email' => $compte->email]) }}" class="link">{{ $compte->email }}</a></td>
+                                        <td class="tableCell" title="Afficher les comptes {{ $compte->name }} lié au mail {{ $compte->email }}"><a href="{{ route('comptes.name.email', ['name' => $compte->name, 'email' => $compte->email]) }}" class="link">{{ $compte->email }}</a></td>
                                     @endif
                                 @else
                                     @if (str_contains(strtolower(URL::current()), 'pseudo'))
-                                        <td class="tableCell" title="Afficher les"><a href="{{ route('comptes.email.pseudo', ['email' => $compte->email, 'pseudo' => $compte->pseudo]) }}" class="link">{{ $compte->email }}</a></td>
+                                        <td class="tableCell" title="Afficher les comptes lié au mail {{ $compte->email }} et avec le pseudo {{ $compte->pseudo }}"><a href="{{ route('comptes.email.pseudo', ['email' => $compte->email, 'pseudo' => $compte->pseudo]) }}" class="link">{{ $compte->email }}</a></td>
                                     @else
-                                        <td class="tableCell" title="Afficher les"><a href="{{ route('comptes.email', ['email' => $compte->email]) }}" class="link">{{ $compte->email }}</a></td>
+                                        <td class="tableCell" title="Afficher les comptes lié au mail {{ $compte->email }}"><a href="{{ route('comptes.email', ['email' => $compte->email]) }}" class="link">{{ $compte->email }}</a></td>
                                     @endif
                                 @endif
                             @endif
                             
                             <!-- Mot de passe -->
                             <td class="tableCell tooltip">
-                                <button title="copié le mot de passe" class="link" onclick="copyToClipboard('{{ str_replace('\'', '\\\'', $compte->password) }}', '{{ $compte->id }}')" onmouseout="tooltip({{ $compte->id }})">
+                                <button title="copier le mot de passe" class="link" onclick="copyToClipboard('{{ str_replace('\'', '\\\'', $compte->password) }}', '{{ $compte->id }}')" onmouseout="tooltip({{ $compte->id }})">
                                     <span class="tooltiptext" id="myTooltip_{{ $compte->id }}">Copier le mot de passe</span>
                                     {{ $compte->password }}
                                 </button>
@@ -116,19 +121,19 @@
                             
                             <!-- Pseudo -->
                             @if (str_contains(strtolower(URL::current()), 'pseudo'))
-                                <td class="tableCell"><a href="{{ route('comptes.pseudo', ['pseudo' => $compte->pseudo]) }}" class="link">{{ $compte->pseudo }}</a></td>
+                                <td class="tableCell"><a title="Afficher les comptes avec le pseudo {{ $compte->pseudo }}" href="{{ route('comptes.pseudo', ['pseudo' => $compte->pseudo]) }}" class="link">{{ $compte->pseudo }}</a></td>
                             @else
                                 @if (str_contains(strtolower(URL::current()), 'email'))
                                     @if (str_contains(strtolower(URL::current()), 'name'))
-                                        <td class="tableCell" title="Afficher les"><a href="{{ route('comptes.name.email.pseudo', ['name' => $compte->name, 'email' => $compte->email, 'pseudo' => $compte->pseudo]) }}" class="link">{{ $compte->pseudo }}</a></td>
+                                        <td class="tableCell" title="Afficher les comptes {{ $compte->name }} lié au mail {{ $compte->email }} avec le pseudo {{ $compte->pseudo }}"><a href="{{ route('comptes.name.email.pseudo', ['name' => $compte->name, 'email' => $compte->email, 'pseudo' => $compte->pseudo]) }}" class="link">{{ $compte->pseudo }}</a></td>
                                     @else
-                                        <td class="tableCell" title="Afficher les"><a href="{{ route('comptes.email.pseudo', ['email' => $compte->email, 'pseudo' => $compte->pseudo]) }}" class="link">{{ $compte->pseudo }}</a></td>
+                                        <td class="tableCell" title="Afficher les comptes lié au mail {{ $compte->email }} et avec le pseudo {{ $compte->pseudo }}"><a href="{{ route('comptes.email.pseudo', ['email' => $compte->email, 'pseudo' => $compte->pseudo]) }}" class="link">{{ $compte->pseudo }}</a></td>
                                     @endif
                                 @else
                                     @if (str_contains(strtolower(URL::current()), 'name'))
-                                        <td class="tableCell" title="Afficher les"><a href="{{ route('comptes.name.pseudo', ['name' => $compte->name, 'pseudo' => $compte->pseudo]) }}" class="link">{{  $compte->pseudo}}</a></td>
+                                        <td class="tableCell" title="Afficher les comptes {{ $compte->name }} avec le pseudo {{ $compte->pseudo }}"><a href="{{ route('comptes.name.pseudo', ['name' => $compte->name, 'pseudo' => $compte->pseudo]) }}" class="link">{{  $compte->pseudo}}</a></td>
                                     @else
-                                        <td class="tableCell" title="Afficher les"><a href="{{ route('comptes.pseudo', ['pseudo' => $compte->pseudo]) }}" class="link">{{ $compte->pseudo }}</a></td>
+                                        <td class="tableCell" title="Afficher les comptes avec le pseudo {{ $compte->pseudo }}"><a href="{{ route('comptes.pseudo', ['pseudo' => $compte->pseudo]) }}" class="link">{{ $compte->pseudo }}</a></td>
                                     @endif
                                 @endif
                             @endif
@@ -136,14 +141,14 @@
                             <!-- Actions -->
                             <td class="smallRowCenterContainer px-1 min-[460px]:px-2 min-[500px]:px-4 py-2 max-sm:hidden">
                                 <!-- Modifier -->
-                                <button onclick="editCompte('{{ str_replace('\'', '\\\'', $compte->name) }}', '{{ str_replace('\'', '\\\'', $compte->email) }}', '{{ str_replace('\'', '\\\'', $compte->password) }}', '{{ str_replace('\'', '\\\'', $compte->pseudo) }}', '{{ $compte->id }}')" class="smallRowCenterContainer w-fit smallTextReverse font-bold bgBleuLogo hover:bgBleuFonce focus:normalScale rounded-lg min-[500px]:rounded-xl py-1 px-1 min-[500px]:px-2">
+                                <button onclick="editCompte('{{ str_replace('\'', '\\\'', $compte->name) }}', '{{ str_replace('\'', '\\\'', $compte->email) }}', '{{ str_replace('\'', '\\\'', $compte->password) }}', '{{ str_replace('\'', '\\\'', $compte->pseudo) }}', '{{ $compte->id }}')" title="Modifier ce compte" class="smallRowCenterContainer w-fit smallTextReverse font-bold bgBleuLogo hover:bgBleuFonce focus:normalScale rounded-lg min-[500px]:rounded-xl py-1 px-1 min-[500px]:px-2">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="tinySizeIcons">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                     </svg>
                                 </button>
 
                                 <!-- Supprimer -->
-                                <a href="{{ route('compte.remove', $compte->id) }}" onclick="return confirm('Êtes-vous sûr de vouloir supprimer le compte {{ str_replace('\'', '\\\'', $compte->name) }} ? Cette action est irréversible.')" class="smallRowCenterContainer w-fit smallTextReverse font-bold bgError hover:bgErrorFonce focus:normalScale rounded-lg min-[500px]:rounded-xl py-1 px-1 min-[500px]:px-2 ml-1 min-[500px]:ml-2">
+                                <a href="{{ route('compte.remove', $compte->id) }}" onclick="return confirm('Êtes-vous sûr de vouloir supprimer le compte {{ str_replace('\'', '\\\'', $compte->name) }} ? Cette action est irréversible.')" title="Supprimer ce compte" class="smallRowCenterContainer w-fit smallTextReverse font-bold bgError hover:bgErrorFonce focus:normalScale rounded-lg min-[500px]:rounded-xl py-1 px-1 min-[500px]:px-2 ml-1 min-[500px]:ml-2">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="tinySizeIcons">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                     </svg>

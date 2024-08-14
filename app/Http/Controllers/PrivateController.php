cac87062e7012106a4b87ade2b53349a6350cef1
FloRobart <FlorisRobart.pro@gmail.com>
2024-08-14 13:43:56 +0200

    correction de bug mineur sur la recherche

--- app/Http/Controllers/PrivateController.php
+++ app/Http/Controllers/PrivateController.php
@@ -626,7 +626,7 @@ class PrivateController extends Controller
 
         /* Recherche des comptes qui contiennent le nom */
         $comptes = $comptes->filter(function ($compte) use ($search) {
-            return str_contains(strtolower($compte->name), $search) || str_contains(strtolower($compte->email), $search) || str_contains(strtolower($compte->pseudo), $search);
+            return str_contains(strtolower($compte->name), strtolower($search)) || str_contains(strtolower($compte->email), strtolower($search)) || str_contains(strtolower($compte->pseudo), strtolower($search));
         });
 
         /* décriptage des mots de passe */

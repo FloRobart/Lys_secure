d1a1893cf81652f0ddddb4173d90eddca07cb41b
FloRobart <FlorisRobart.pro@gmail.com>
2024-12-15 16:53:03 +0100

    correction affichage PP dans le header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -23,7 +23,7 @@
                 <!-- Icône du profil -->
                 @if (auth()->check())
                     @if (auth()->user()->imgProfil !== null)
-                        <img class="h-12 lg:h-16 xl:h-20 overflow-hidden rounded-full" src="data:image/png;base64,{{ auth()->user()->imgProfil }}" alt="Image de profil">
+                        <img class="w-12 h-12 lg:w-16 lg:h-16 xl:w-20 xl:h-20 overflow-hidden rounded-full" src="data:image/png;base64,{{ auth()->user()->imgProfil }}" alt="Image de profil">
                     @else
                         <svg class="normalIcons colorFontBleuLogo" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />

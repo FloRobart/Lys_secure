d35d049c2ce5ea09f63caa07fce351fe7209d726
FloRobart <FlorisRobart.pro@gmail.com>
2024-12-17 16:06:19 +0100

    suppression temporaire des logs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,12 +17,12 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->respond(function (Response $response) {
-            LogController::addLog('Une erreur critique est survenue', null, 1);
-            if (env('APP_ENV') == 'local') {
-                return $response;
-            }
+        // $exceptions->respond(function (Response $response) {
+        //     LogController::addLog('Une erreur critique est survenue', null, 1);
+        //     if (env('APP_ENV') == 'local') {
+        //         return $response;
+        //     }
 
-            return Redirect()->route('accueil')->with('error', 'La page demandée à rencontrée une erreur. L\'administrateur à été informé du problème et travail à le résoudre. Vous serez contacter quand le problème sera résolu.');
-        });
+        //     return Redirect()->route('accueil')->with('error', 'La page demandée à rencontrée une erreur. L\'administrateur à été informé du problème et travail à le résoudre. Vous serez contacter quand le problème sera résolu.');
+        // });
     })->create();

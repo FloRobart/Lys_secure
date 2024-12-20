2dfb3dccaba0fdc8fecb015a8866e9d793afdfc2
FloRobart <FlorisRobart.pro@gmail.com>
2024-12-21 00:17:04 +0100

    correction de bug

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -9,10 +9,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Account extends Model
 {
     use HasFactory;
 
+    public $timestamps = false;
     protected $connection = 'account_manager';
     protected $table = 'accounts';
 
--- app/Models/Key.php
+++ app/Models/Key.php
@@ -9,10 +9,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Key extends Model
 {
     use HasFactory;
 
+    public $timestamps = false;
     protected $connection = 'account_manager';
     protected $table = 'keys';
 
